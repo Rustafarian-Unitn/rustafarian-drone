e768d2533aed2ab0aeee108f91050678dcc542e9
Lorenzo <lorenzofumi@gmail.com>
2024-12-08 15:48:08 +0100

    tests: check ControllerShortcut in case the channel is closed

--- src/tests/fragments.rs
+++ src/tests/fragments.rs
@@ -2,13 +2,16 @@
 #[cfg(test)]
 mod fragment_tests {
     use crate::RustafarianDrone;
+    use crate::SourceRoutingHeader;
     use crossbeam_channel::{unbounded, Receiver, Sender};
     use std::collections::HashMap;
     use std::thread;
-    use wg_2024::packet::{FloodRequest, FloodResponse, Fragment, Nack, NackType, NodeType, Packet, PacketType};
-    use wg_2024::tests;
-    use crate::SourceRoutingHeader;
+    use wg_2024::controller::DroneEvent;
     use wg_2024::drone::Drone;
+    use wg_2024::packet::{
+        FloodRequest, FloodResponse, Fragment, Nack, NackType, NodeType, Packet, PacketType,
+    };
+    use wg_2024::tests;
 
     /// Check that a message is forwarded correctly
     #[test]
@@ -253,9 +256,7 @@ mod fragment_tests {
         let (d1_send, d1_recv) = unbounded();
         let (_d_command_send, d_command_recv) = unbounded();
 
-        let neighbours1 = HashMap::from([
-            (1, c_send.clone()),
-        ]);
+        let neighbours1 = HashMap::from([(1, c_send.clone())]);
 
         let mut drone1 = RustafarianDrone::new(
             11,
@@ -277,17 +278,20 @@ mod fragment_tests {
             session_id: 1,
             routing_header: SourceRoutingHeader {
                 hop_index: 1,
-                hops: [11, 1].to_vec()
+                hops: [11, 1].to_vec(),
             },
             pack_type: PacketType::Nack(Nack {
                 fragment_index: 1,
-                nack_type: NackType::ErrorInRouting(12)
-            })
+                nack_type: NackType::ErrorInRouting(12),
+            }),
         };
 
-        assert_eq!(c_recv.recv().unwrap(), expected_nack, "The message received should be an error in routing!");
+        assert_eq!(
+            c_recv.recv().unwrap(),
+            expected_nack,
+            "The message received should be an error in routing!"
+        );
     }
-
 
     /// Test correct behavior in case the destination is a drone!
     #[test]
@@ -297,9 +301,7 @@ mod fragment_tests {
         let (d1_send, d1_recv) = unbounded();
         let (_d_command_send, d_command_recv) = unbounded();
 
-        let neighbours1 = HashMap::from([
-            (1, c_send.clone()),
-        ]);
+        let neighbours1 = HashMap::from([(1, c_send.clone())]);
 
         let mut drone1 = RustafarianDrone::new(
             11,
@@ -318,14 +320,14 @@ mod fragment_tests {
             session_id: 0,
             routing_header: SourceRoutingHeader {
                 hop_index: 1,
-                hops: [1, 11].to_vec()
+                hops: [1, 11].to_vec(),
             },
             pack_type: PacketType::MsgFragment(Fragment {
                 fragment_index: 0,
                 total_n_fragments: 1,
                 length: 1,
-                data: [2; 128]
-            })
+                data: [2; 128],
+            }),
         };
 
         d1_send.send(msg).unwrap();
@@ -333,17 +335,22 @@ mod fragment_tests {
             session_id: 0,
             routing_header: SourceRoutingHeader {
                 hop_index: 1,
-                hops: [11, 1].to_vec()
+                hops: [11, 1].to_vec(),
             },
             pack_type: PacketType::Nack(Nack {
                 fragment_index: 0,
-                nack_type: NackType::DestinationIsDrone
-            })
+                nack_type: NackType::DestinationIsDrone,
+            }),
         };
 
-        assert_eq!(c_recv.recv().unwrap(), expected_nack, "I should receive a NACK!");
+        assert_eq!(
+            c_recv.recv().unwrap(),
+            expected_nack,
+            "I should receive a NACK!"
+        );
     }
 
+    /// The hops index identifies another drone!
     #[test]
     fn run_wrong_hop() {
         let (c_send, c_recv) = unbounded();
@@ -351,9 +358,7 @@ mod fragment_tests {
         let (d1_send, d1_recv) = unbounded();
         let (_d_command_send, d_command_recv) = unbounded();
 
-        let neighbours1 = HashMap::from([
-            (1, c_send.clone()),
-        ]);
+        let neighbours1 = HashMap::from([(1, c_send.clone())]);
 
         let mut drone1 = RustafarianDrone::new(
             11,
@@ -372,14 +377,14 @@ mod fragment_tests {
             session_id: 0,
             routing_header: SourceRoutingHeader {
                 hop_index: 1,
-                hops: [1, 12, 13, 11].to_vec() // Here, 12 instead of 11
+                hops: [1, 12, 13, 11].to_vec(), // Here, 12 instead of 11
             },
             pack_type: PacketType::MsgFragment(Fragment {
                 fragment_index: 0,
                 total_n_fragments: 1,
                 length: 1,
-                data: [2; 128]
-            })
+                data: [2; 128],
+            }),
         };
 
         d1_send.send(msg).unwrap();
@@ -388,14 +393,67 @@ mod fragment_tests {
             session_id: 0,
             routing_header: SourceRoutingHeader {
                 hop_index: 1,
-                hops: [11, 1].to_vec()
+                hops: [11, 1].to_vec(),
             },
             pack_type: PacketType::Nack(Nack {
                 fragment_index: 0,
-                nack_type: NackType::UnexpectedRecipient(11)
-            })
+                nack_type: NackType::UnexpectedRecipient(11),
+            }),
         };
 
-        assert_eq!(c_recv.recv().unwrap(), expected_nack, "I should receive a NACK!");
+        assert_eq!(
+            c_recv.recv().unwrap(),
+            expected_nack,
+            "I should receive a NACK!"
+        );
+    }
+
+    /// Test that when the channel is closed, the drone sends a ControllerShortcut
+    #[test]
+    fn run_closed_channel() {
+        let (c_send, c_recv) = unbounded();
+        let (s_send, s_recv) = unbounded::<Packet>();
+        let (d1_send, d1_recv) = unbounded();
+        let (d2_send, d2_recv) = unbounded();
+        let (_d_command_send, d_command_recv) = unbounded();
+        let (d_command_send, _d_command_recv) = unbounded();
+
+        let neighbours1 = HashMap::from([(12, d2_send.clone()), (1, c_send.clone())]);
+
+        let mut drone1 = RustafarianDrone::new(
+            11,
+            d_command_send,
+            d_command_recv.clone(),
+            d1_recv.clone(),
+            neighbours1,
+            0.0,
+        );
+
+        thread::spawn(move || {
+            drone1.run();
+        });
+
+        let mut msg = Packet {
+            session_id: 0,
+            routing_header: SourceRoutingHeader {
+                hop_index: 1,
+                hops: [1, 11, 12].to_vec(),
+            },
+            pack_type: PacketType::Ack(wg_2024::packet::Ack { fragment_index: 0 }),
+        };
+
+        drop(d2_send);
+        drop(d2_recv);
+        d1_send.send(msg.clone()).unwrap();
+
+        msg.routing_header.hop_index += 1;
+
+        let expected_shortcut = DroneEvent::ControllerShortcut(msg);
+
+        assert_eq!(
+            _d_command_recv.recv().unwrap(),
+            expected_shortcut,
+            "I should receive a ControllerShortcut!"
+        );
     }
 }
