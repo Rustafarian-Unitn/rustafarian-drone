0e89cced6a03506f64c3389066a63e2ddc721e83
Lorenzo <lorenzofumi@gmail.com>
2024-12-08 14:33:26 +0100

    tests: added nack forwarding tests from client to drone

--- src/tests/fragments.rs
+++ src/tests/fragments.rs
@@ -166,4 +166,82 @@ mod fragment_tests {
             session_id: 1,
         }
     }
+
+    /// Check that the NACK is forwarded correctly.
+    #[test]
+    fn run_nack() {
+        let (c_send, c_recv) = unbounded();
+        let (s_send, s_recv) = unbounded();
+        let (d1_send, d1_recv) = unbounded();
+        let (d2_send, d2_recv) = unbounded();
+        let (d3_send, d3_recv) = unbounded();
+        let (_d_command_send, d_command_recv) = unbounded();
+
+        let neighbours1 = HashMap::from([
+            (12, d2_send.clone()),
+            (13, d3_send.clone()),
+            (1, c_send.clone()),
+        ]);
+        let neighbours2 = HashMap::from([
+            (11, d1_send.clone()),
+            (13, d3_send.clone()),
+            (21, s_send.clone()),
+        ]);
+        let neighbours3 = HashMap::from([(11, d1_send.clone()), (12, d2_send.clone())]);
+
+        let mut drone1 = RustafarianDrone::new(
+            11,
+            unbounded().0,
+            d_command_recv.clone(),
+            d1_recv.clone(),
+            neighbours1,
+            0.0,
+        );
+        let mut drone2 = RustafarianDrone::new(
+            12,
+            unbounded().0,
+            d_command_recv.clone(),
+            d2_recv.clone(),
+            neighbours2,
+            0.0,
+        );
+        let mut drone3 = RustafarianDrone::new(
+            13,
+            unbounded().0,
+            d_command_recv.clone(),
+            d3_recv.clone(),
+            neighbours3,
+            0.0,
+        );
+
+        thread::spawn(move || {
+            drone1.run();
+        });
+        thread::spawn(move || {
+            drone2.run();
+        });
+        thread::spawn(move || {
+            drone3.run();
+        });
+
+        let dropped = Nack {
+            fragment_index: 1,
+            nack_type: NackType::Dropped,
+        };
+        let srh = SourceRoutingHeader {
+            hop_index: 1,
+            hops: vec![1, 11, 13, 12, 21],
+        };
+        let mut nack_packet = Packet {
+            pack_type: PacketType::Nack(dropped),
+            routing_header: srh,
+            session_id: 1,
+        };
+
+        d1_send.send(nack_packet.clone()).unwrap();
+
+        nack_packet.routing_header.hop_index += 3;
+
+        assert_eq!(s_recv.recv().unwrap(), nack_packet);
+    }
 }
