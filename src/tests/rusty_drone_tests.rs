8ec665be8e8684b9c951bbb9a6e29398dc7f9642
eneastrambini <strambinienea18@gmail.com>
2025-01-05 15:01:11 +0100

    fix: fix drone crashing behaviour, now should be compliant with drone protocol. Add test from rusty-drone group.

---
+++ src/tests/rusty_drone_tests.rs
@@ -0,0 +1,68 @@
+#![cfg(test)]
+use rusty_tester::*;
+use std::time::Duration;
+use crate::RustafarianDrone;
+
+type Tested = RustafarianDrone;
+const TIMEOUT: Duration = Duration::from_millis(100);
+const FLOOD_TIMEOUT: Duration = Duration::from_millis(100);
+
+#[test]
+fn drone_destination_is_drone() {
+    test_drone_destination_is_drone::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_error_in_routing() {
+    test_drone_error_in_routing::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_packet_1_hop() {
+    test_drone_packet_1_hop::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_packet_3_hop() {
+    test_drone_packet_3_hop::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_packet_3_hop_crash() {
+    test_drone_packet_3_hop_crash::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn easiest_flood() {
+    test_easiest_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn loop_flood() {
+    test_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn hard_loop_flood() {
+    test_hard_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn matrix_loop_flood() {
+    test_matrix_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn star_loop_flood() {
+    test_star_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn butterfly_loop_flood() {
+    test_butterfly_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn tree_loop_flood() {
+    test_tree_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
